5d15f16e93a3bebd84722a09bec7072f771fce6b
Anju Krishna <anjukrishna414@gmail.com>
2026-02-15 22:30:55 -0500

    Final Message

--- sample1/src/main/java/Super_keyword/cat.java
+++ sample1/src/main/java/Super_keyword/cat.java
@@ -5,7 +5,7 @@ public class cat extends animal {
 	int leg_number=6;
 	public void display() {
 		
-		System.out.println("PARENT CLASS ANIMAL LEG="+super.leg_number);//point parent class variable
+		System.out.println("Print PARENT CLASS ANIMAL LEG="+super.leg_number);//point parent class variable
 		System.out.println("Child CLASS ANIMAL LEG="+leg_number);
 	}
 
